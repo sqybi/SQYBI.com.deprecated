895410a2f2d6b0daa4dc715ac74fe4c343ecdaa7
sqybi <sqybi@126.com>
2014-08-16 16:41:56 +0000

    Some view logic

--- dynamic/blog/views.py
+++ dynamic/blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from blog.models import Article
+
 # Create your views here.
 def index(request):
     return HttpResponse("Test View, this is the index page of SQYBI.com blog.");
 
 def article(request, article_id = None, article_slug = None):
     if article_id != None:
-        return HttpResponse("Request with article_id %s" % article_id)
+        article = Article.objects.get(id=article_id)
+    elif article_slug != None:
+        article = Article.objects.get(slug=article_slug)
     else:
-        return HttpResponse("Request with article_slug %s" % article_slug)
+        return HttpResponse("Error request!")
+    return HttpResponse("<h3>%s</h3><p>%s</p>" % (article.title, article.html_content))
 
