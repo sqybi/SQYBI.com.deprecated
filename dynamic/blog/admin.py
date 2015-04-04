d51f332e1540f5be1a7295e0865c4fc92c694220
sqybi <sqybi@126.com>
2015-04-04 18:51:17 +0800

    bug fix

--- dynamic/blog/admin.py
+++ dynamic/blog/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 import blog.models
 import website.models
+import user.models
 
 
 # Register your models here.
-admin.site.register(website.models.User)
-admin.site.register(website.models.UserRecord)
-admin.site.register(blog.models.Article)
-admin.site.register(blog.models.Comment)
+admin.site.register(user.models.User)
+admin.site.register(user.models.UserRecord)
+admin.site.register(website.models.BaseItem)
+admin.site.register(blog.models.BlogArticleItem)
+admin.site.register(blog.models.BlogCommentItem)
