057045e9926acf15aa5e1946301c9bbd69dffa00
sqybi <sqybi@126.com>
2015-04-04 20:06:03 +0800

    begin blog page work, add alert message and move some constants to constants.py

--- dynamic/blog/views.py
+++ dynamic/blog/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
 from blog.models import BlogArticleItem
+import helper.constants
 
 
 def index(request, page=1):
-    articles = BlogArticleItem.objects.filter(is_shown=True).order_by("id")[::-1]
+    articles = BlogArticleItem.objects.filter(is_shown=True).order_by("create_time")[::-1]
 
     start_article = (page - 1) * 5;
     end_article = page * 5;
 
     context = {
-        "articles": list(articles),
-        "selected_articles": list(articles)[start_article:end_article],
+        "title": "三千院大小姐的紫公馆 | SQYBI.com",
         "app": "blog",
+        "alert_level": helper.constants.alert_level,
+        "alert_message": helper.constants.alert_message,
+        "all_articles": list(articles),
+        "selected_articles": list(articles)[start_article:end_article],
     }
 
     return render(request, "blog/index.htm", context)
@@ -23,16 +27,19 @@ def article(request, article_id=None, article_slug=None):
     articles = BlogArticleItem.objects.filter(is_shown=True).order_by("id")[::-1]
 
     if article_id is not None:
-        article_object = get_object_or_404(BlogArticleItem, id=article_id)
+        selected_article = get_object_or_404(BlogArticleItem, id=article_id)
     elif article_slug is not None:
-        article_object = get_object_or_404(BlogArticleItem, slug=article_slug)
+        selected_article = get_object_or_404(BlogArticleItem, slug=article_slug)
     else:
         raise Http404
 
     context = {
-        "articles": list(articles),
-        "article": article_object,
+        "title": selected_article.title + " | 三千院大小姐的紫公馆 | SQYBI.com",
         "app": "blog",
+        "alert_level": helper.constants.alert_level,
+        "alert_message": helper.constants.alert_message,
+        "all_articles": list(articles),
+        "selected_article": selected_article,
     }
 
     return render(request, "blog/article.htm", context)
--- dynamic/helper/constants.py
+++ dynamic/helper/constants.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """
 Settings for the whole website.
 
@@ -16,3 +18,18 @@ db_password = "000000"
 
 # Django secret key
 django_secret_key = '=erw^b=h7gqntcy%x%fqg*f%mj3hl8odxg7kp@h6hwu647blh$'
+
+# Website alert
+alert_level = None
+alert_message = None
+
+# Daily Quotes
+daily_quotes = [
+    u"""“大家都很正常地怀有着梦想呢……”<br/>
+“没错没错，大家都在朝着梦想拼命奋斗呢。这就是所谓的活着哦。”<br/>
+“梦想啊，目标啊之类的，根本用不着吧？”<br/>
+“为什么总说这种冷峻无情的话呢！”<br/>
+“因为你不觉得很奇怪吗，只有那些成天念叨着梦想的人，才看不清现实啦。”""",
+    u"""我从不认为这些年的青春被狗吃了，以我对狗的了解，他们才不会吃这么没营养的东西。""",
+    u"""人类一生中最绚烂的时刻，就是面对着失败的命运顽强抗争，最终彻底地失败。""",
+]
