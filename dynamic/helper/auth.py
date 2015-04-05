a4da972b0382425487e29803f3d377b11a30b3d1
sqybi <sqybi@126.com>
2015-04-05 21:00:07 +0800

    update token if it is retrieved from cookie

--- dynamic/helper/auth.py
+++ dynamic/helper/auth.py
@@ -4,6 +4,7 @@ Helper methods related to Authorization
 
 from user.models import User, UserRecord
 import helper.constants
+import helper.security
 
 
 def get_current_user(request):
@@ -13,7 +14,7 @@ def get_current_user(request):
     if "user_name" in request.session and "token" in request.session:
         user_name = request.session["user_name"]
         token = request.session["token"]
-        user = get_user(user_name, token)
+        user = get_user(user_name, token)[0]
 
     if user is not None:
         return user
@@ -22,17 +23,27 @@ def get_current_user(request):
     user_name = request.COOKIES.get("user_name")
     token = request.COOKIES.get("token")
     if user_name is not None and token is not None:
-        user = get_user(user_name, token)
+        result = get_user(user_name, token)
+        user = result[0]
+        user_record = result[1]
+        if user is not None:
+            # get new cookie token and update
+            user_record.cookie_token = helper.security.generate_cookie_token(user.user_name)
+            user_record.save()
+            request.COOKIES["token"] = user_record.cookie_token
+            request.session["token"] = user_record.cookie_token
 
     return user
 
 
 def get_user(user_name, token):
     user = None
+    user_record = None
 
     if helper.constants.user_name_pattern.match(user_name):
         user = User.objects.filter(user_name=user_name).first()
-        if UserRecord.objects.filter(user=user).first().cookie_token != token:
+        user_record = UserRecord.objects.filter(user=user).first()
+        if user_record is None or user_record.cookie_token != token:
             user = None
 
-    return user
+    return user, user_record
