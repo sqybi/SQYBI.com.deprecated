89207190eb3885e5977707636f58574f1da2a321
sqybi <sqybi@126.com>
2015-04-05 19:15:21 +0800

    change blog article urls name to article

--- dynamic/blog/urls.py
+++ dynamic/blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns(
     "",
     url(r"^$", views.index, name="index"),
     url(r"^(?P<page>\d+)/$", views.index, name="article_page"),
-    url(r"^article/(?P<article_id>\d+)/$", views.article, name="article_id"),
-    url(r"^article/(?P<article_slug>.+)/$", views.article, name="article_slug"),
+    url(r"^article/(?P<article_id>\d+)/$", views.article, name="article"),
+    url(r"^article/(?P<article_slug>.+)/$", views.article, name="article"),
 )
