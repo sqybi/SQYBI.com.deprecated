546be3fdac331ebb5491c7caad984cd92281e071
sqybi <sqybi@126.com>
2015-04-05 18:28:29 +0800

    resolve the conflict between auth app and admin/auth app

---
+++ dynamic/auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AuthConfig(AppConfig):
+    name = "site_auth"
+    verbose_name = "This site's auth app"
--- dynamic/website/settings.py
+++ dynamic/website/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
     "website",
     "blog",
     "user",
-    "auth",
+    "site_auth",
 )
 
 MIDDLEWARE_CLASSES = (
