c4ad9217a146ebb9b38fa4cf028f8dbbe6c8752f
sqybi <sqybi@126.com>
2015-04-05 14:02:28 +0800

    add blog article page

--- dynamic/blog/views.py
+++ dynamic/blog/views.py
@@ -6,29 +6,37 @@ import helper.constants
 
 
 def index(request, page=1):
-    articles = BlogArticleItem.objects.filter(is_shown=True).order_by("create_time")[::-1]
+    recent_articles = list(
+        BlogArticleItem.objects.filter(is_shown=True).order_by("id").reverse()[:helper.constants.blog_article_per_page])
 
     start_article = (page - 1) * helper.constants.blog_article_per_page;
     end_article = page * helper.constants.blog_article_per_page;
 
-    total_page = (len(articles) - 1 + helper.constants.blog_article_per_page) / helper.constants.blog_article_per_page
+    total_article_count = BlogArticleItem.objects.filter(is_shown=True).count()
+
+    total_page_count = (
+                       total_article_count - 1 + helper.constants.blog_article_per_page) / helper.constants.blog_article_per_page
+
+    selected_articles = list(
+        BlogArticleItem.objects.filter(is_shown=True).order_by("id").reverse()[start_article:end_article])
 
     context = {
         "title": helper.constants.blog_title + " | SQYBI.com",
         "app": "blog",
         "alert_level": helper.constants.alert_level,
         "alert_message": helper.constants.alert_message,
-        "all_articles": list(articles),
-        "selected_articles": list(articles)[start_article:end_article],
+        "recent_articles": recent_articles,
+        "selected_articles": selected_articles,
         "prev_page": None if page <= 1 else str(page - 1),
-        "next_page": None if page >= total_page else str(page + 1),
+        "next_page": None if page >= total_page_count else str(page + 1),
     }
 
     return render(request, "blog/index.html", context)
 
 
 def article(request, article_id=None, article_slug=None):
-    articles = BlogArticleItem.objects.filter(is_shown=True).order_by("id")[::-1]
+    recent_articles = list(
+        BlogArticleItem.objects.filter(is_shown=True).order_by("id").reverse()[:helper.constants.blog_article_per_page])
 
     if article_id is not None:
         selected_article = get_object_or_404(BlogArticleItem, id=article_id)
@@ -37,13 +45,19 @@ def article(request, article_id=None, article_slug=None):
     else:
         raise Http404
 
+    previous_article = BlogArticleItem.objects.filter(id__lt=selected_article.id, is_shown=True).order_by(
+        "id").reverse().first()
+    next_article = BlogArticleItem.objects.filter(id__gt=selected_article.id, is_shown=True).order_by("id").first()
+
     context = {
         "title": selected_article.title + " | " + helper.constants.blog_title + " | SQYBI.com",
         "app": "blog",
         "alert_level": helper.constants.alert_level,
         "alert_message": helper.constants.alert_message,
-        "all_articles": list(articles),
+        "recent_articles": recent_articles,
         "selected_article": selected_article,
+        "prev_article": previous_article,
+        "next_article": next_article,
     }
 
     return render(request, "blog/article.html", context)
