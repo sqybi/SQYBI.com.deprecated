011daa79fe51453ddb2fd3a50eb656e9057cab27
sqybi <sqybi@126.com>
2015-04-06 15:42:07 +0800

    about page

---
+++ dynamic/about/views.py
@@ -0,0 +1,20 @@
+import django.http
+
+import helper.auth
+import helper.general
+
+
+def index(request):
+    if request.method == "GET":
+        user = helper.auth.get_current_user(request)
+
+        context = {
+            "title": "About | SQYBI.com",
+            "app": "about",
+            "user": user,
+            "request": request,
+            "alert_level": helper.general.get_alert_level(request),
+            "alert_message": helper.general.get_alert_message(request),
+        }
+    else:
+        raise django.http.Http404
