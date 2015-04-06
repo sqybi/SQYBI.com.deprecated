d5000f9c26df163c9ec913904e940e34d5533784
sqybi <sqybi@126.com>
2015-04-06 20:02:46 +0800

    try to fix foreign key issue

--- dynamic/website/models.py
+++ dynamic/website/models.py
@@ -10,7 +10,7 @@ import user.models
 class BaseItem(models.Model):
     create_time = models.DateTimeField()
     modify_time = models.DateTimeField()
-    author = models.ForeignKey(user.models.User, null=True, on_delete=models.SET_NULL, related_name="%(class)s")
+    author = models.ForeignKey(user.models.User, null=True, on_delete=models.SET_NULL, related_name="%(class)ss")
 
     class Meta():
         abstract = True
